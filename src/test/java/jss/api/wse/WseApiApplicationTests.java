aded766b7744641794d4b5dabe03363f5e8b0338
Jonatan Silva <jonatan.tab58@gmail.com>
2022-02-16 16:51:04 -0300

    🎉 Initial commit

---
+++ src/test/java/jss/api/wse/WseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jss.api.wse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
