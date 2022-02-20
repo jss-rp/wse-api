a06728a26ed0f629b9a0cbb8c4c742376fcdc05b
Jonatan Silva <jonatan.tab58@gmail.com>
2022-02-20 19:41:16 -0300

    🔉 Logs for controller responses

--- src/main/java/jss/api/wse/controller/TrendController.java
+++ src/main/java/jss/api/wse/controller/TrendController.java
@@ -3,6 +3,8 @@ package jss.api.wse.controller;
 import jss.api.wse.model.Trend;
 import jss.api.wse.service.TrendService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -12,14 +14,14 @@ import reactor.core.publisher.Mono;
 @RequestMapping("trends")
 public class TrendController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TrendController.class);
     private final TrendService trendService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Trend> createTrend(@RequestBody Mono<Trend> trend) {
         return trend.flatMap(trendService::create)
-                .doOnSuccess(createdTrend -> Mono.just(createdTrend))
-                .doOnError(error -> Mono.error(new RuntimeException("Falha ao criar trend")));
-
+                .doOnSuccess(created -> logger.info(created.toString()))
+                .doOnError(error -> logger.debug("Fail to create a trend", error.fillInStackTrace()));
     }
 }
